642debf2c8f1e2dd0ca4888da52be8bd35785405
HeitorBMarini <mariniheitor@gmail.com>
2025-03-08 14:44:25 -0300

    Add clique para listar os detalhes dos usuarios

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,20 +1,24 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Importa para usar *ngFor
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { IUser } from '../../interfaces/user/user.interface';
 import { UserList } from '../../data/user-list';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CommonModule], // Importa para o Angular reconhecer *ngFor
+  imports: [CommonModule],
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
 export class TableComponent {
-  displayedColumns: string[] = ['nome', 'dataCadastro', 'funcao' ,'status'];
+  displayedColumns: string[] = ['nome', 'dataCadastro','funcao', 'status'];
   userList: IUser[] = UserList;
 
-  getStatus(user: IUser): string {
-    return user.status.online ? "Ativo" : "Inativo";
+  // Evento para enviar os dados do usuário selecionado para o componente pai
+  @Output() userSelected = new EventEmitter<IUser>();
+
+  // Método que emite os dados do usuário ao clicar
+  selectUser(user: IUser) {
+    this.userSelected.emit(user);
   }
 }
--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Necessário para ngClass
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IUser } from '../../interfaces/user/user.interface';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [CommonModule], // Importa CommonModule para ngClass
+  imports: [CommonModule],
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent {
   isSidebarOpen: boolean = false;
 
+  // Recebe os dados do usuário da tabela
+  @Input() user: IUser | null = null;
+
+  // Método chamado ao receber um novo usuário
+  openSidebar(user: IUser) {
+    this.user = user;
+    this.isSidebarOpen = true;
+  }
+
+  // Fecha o sidebar
   toggleSidebar() {
-    this.isSidebarOpen = !this.isSidebarOpen;
+    this.isSidebarOpen = false;
   }
 }
+
