c199d02511581bff11f4593f5424faf1cecc2a1d
HeitorBMarini <mariniheitor@gmail.com>
2025-03-09 13:45:39 -0300

    Ajustes front end

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,25 +1,31 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IUser } from '../../interfaces/user/user.interface';
+import { EmptyValuePipe } from "../../pipes/empty-value.pipe";
+import { PhonePipe } from "../../pipes/phone.pipe";
+import { StatusPipe } from "../../pipes/status.pipe";
+import { DatePipeCustom } from "../../pipes/date.pipe";
+import { AddressPipe } from "../../pipes/address.pipe";
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, EmptyValuePipe, PhonePipe, StatusPipe, DatePipeCustom, AddressPipe],
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent {
   isSidebarOpen: boolean = false;
+  @Input() user: IUser | null = null;
 
-  @Input() user: IUser | null = null; // Permite receber null
-
-  openSidebar(user: IUser | null) {
-    this.user = user;
-    this.isSidebarOpen = !!user; // Se user for null, fecha o sidebar
+  // Método para abrir manualmente
+  openSidebar(user?: IUser | null) {
+    this.user = user ?? this.user; // Mantém o usuário anterior se `user` for undefined
+    this.isSidebarOpen = true; 
   }
 
+  // Método para fechar manualmente
   toggleSidebar() {
-    this.isSidebarOpen = false;
+    this.isSidebarOpen = !this.isSidebarOpen;
   }
 }
